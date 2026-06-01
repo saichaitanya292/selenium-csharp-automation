f86d2dd85a3eafdf4e5e978a45afcb056b9a7b3c
Bscg <114502884+saichaitanya292@users.noreply.github.com>
2026-06-01 15:23:40 +0530

    Fix: Add missing using directives for List and Linq in DropdownPage

--- Pages/DropdownPage.cs
+++ Pages/DropdownPage.cs
@@ -1,5 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Linq;
 using SeleniumAutomation.Config;
 
 namespace SeleniumAutomation.Pages
